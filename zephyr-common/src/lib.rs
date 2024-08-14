2a33df0616444afdd9bb8362a2ad84e56a4fe5cc
heytdep <tommaso@xycloo.com>
2024-08-14 09:31:49 +0200

    add intra-number deserializations guest-side

--- zephyr-common/src/lib.rs
+++ zephyr-common/src/lib.rs
@@ -116,14 +116,18 @@ pub struct Account {
     pub num_sponsoring: i32,
 }
 
-macro_rules! impl_inner_from {
+macro_rules! impl_inner_from_serialize_only {
     ($variant:ident, $inner:ty) => {
         impl From<$inner> for ZephyrVal {
             fn from(value: $inner) -> Self {
                 ZephyrVal::$variant(value)
             }
         }
+    };
+}
 
+macro_rules! impl_inner_from_deserialize_generic {
+    ($variant:ident, $inner:ty) => {
         impl From<ZephyrVal> for $inner {
             fn from(value: ZephyrVal) -> Self {
                 match value {
@@ -135,15 +139,47 @@ macro_rules! impl_inner_from {
     };
 }
 
-impl_inner_from!(I128, i128);
-impl_inner_from!(I64, i64);
-impl_inner_from!(U64, u64);
-impl_inner_from!(F64, f64);
-impl_inner_from!(U32, u32);
-impl_inner_from!(I32, i32);
-impl_inner_from!(F32, f32);
-impl_inner_from!(String, String);
-impl_inner_from!(Bytes, Vec<u8>);
+macro_rules! impl_inner_from_deserialize_numeric {
+    ($inner:ty) => {
+        impl From<ZephyrVal> for $inner {
+            fn from(value: ZephyrVal) -> Self {
+                match value {
+                    //ZephyrVal::F32(num) => num as $inner,
+                    //ZephyrVal::F64(num) => num as $inner,
+                    ZephyrVal::I128(num) => num as $inner,
+                    ZephyrVal::I32(num) => num as $inner,
+                    ZephyrVal::I64(num) => num as $inner,
+                    ZephyrVal::U32(num) => num as $inner,
+                    ZephyrVal::U64(num) => num as $inner,
+                    _ => panic!("Attempted to convert ZephyrVal variant to different inner type"),
+                }
+            }
+        }
+    };
+}
+
+// Ser
+impl_inner_from_serialize_only!(I128, i128);
+impl_inner_from_serialize_only!(I64, i64);
+impl_inner_from_serialize_only!(U64, u64);
+impl_inner_from_serialize_only!(F64, f64);
+impl_inner_from_serialize_only!(U32, u32);
+impl_inner_from_serialize_only!(I32, i32);
+impl_inner_from_serialize_only!(F32, f32);
+impl_inner_from_serialize_only!(String, String);
+impl_inner_from_serialize_only!(Bytes, Vec<u8>);
+
+// Deser
+impl_inner_from_deserialize_numeric!(i128);
+impl_inner_from_deserialize_numeric!(i64);
+impl_inner_from_deserialize_numeric!(u64);
+impl_inner_from_deserialize_numeric!(u32);
+impl_inner_from_deserialize_numeric!(i32);
+impl_inner_from_deserialize_generic!(String, String);
+impl_inner_from_deserialize_generic!(Bytes, Vec<u8>);
+impl_inner_from_deserialize_generic!(F64, f64);
+impl_inner_from_deserialize_generic!(F32, f32);
+
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub enum RelayedMessageRequest {
